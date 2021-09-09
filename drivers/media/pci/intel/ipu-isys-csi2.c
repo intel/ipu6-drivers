bdd4a31fa7b1426f457493ed81665b07d5d8cb0d
Xu Chongyang <chongyang.xu@intel.com>
2021-09-09 10:00:12 +0800

    update at WW37.4
    Signed-off-by: Xu Chongyang <chongyang.xu@intel.com>

--- drivers/media/pci/intel/ipu-isys-csi2.c
+++ drivers/media/pci/intel/ipu-isys-csi2.c
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2013 - 2020 Intel Corporation
+// Copyright (C) 2013 - 2021 Intel Corporation
 
 #include <linux/device.h>
 #include <linux/module.h>
 #include <linux/version.h>
+#ifdef CONFIG_USE_CVF_DEVICE
+#include <linux/vsc.h>
+#endif
 
 #include <media/ipu-isys.h>
 #include <media/media-entity.h>
@@ -224,6 +227,11 @@ static int set_stream(struct v4l2_subdev *sd, int enable)
 	struct ipu_isys_csi2_timing timing = {0};
 	unsigned int nlanes;
 	int rval;
+#ifdef CONFIG_USE_CVF_DEVICE
+	struct vsc_mipi_config conf;
+	struct vsc_camera_status status;
+	s64 link_freq;
+#endif
 
 	dev_dbg(&csi2->isys->adev->dev, "csi2 s_stream %d\n", enable);
 
@@ -236,6 +244,14 @@ static int set_stream(struct v4l2_subdev *sd, int enable)
 
 	if (!enable) {
 		ipu_isys_csi2_set_stream(sd, timing, 0, enable);
+#ifdef CONFIG_USE_CVF_DEVICE
+		rval = vsc_release_camera_sensor(&status);
+		if (rval) {
+			dev_err(&csi2->isys->adev->dev,
+				"Release VSC failed.\n");
+			return rval;
+		}
+#endif
 		return 0;
 	}
 
@@ -250,6 +266,20 @@ static int set_stream(struct v4l2_subdev *sd, int enable)
 		return rval;
 
 	rval = ipu_isys_csi2_set_stream(sd, timing, nlanes, enable);
+#ifdef CONFIG_USE_CVF_DEVICE
+	rval = ipu_isys_csi2_get_link_freq(csi2, &link_freq);
+	if (rval)
+		return rval;
+
+	conf.lane_num = nlanes;
+	/* frequency unit 100k */
+	conf.freq = link_freq / 100000;
+	rval = vsc_acquire_camera_sensor(&conf, NULL, NULL, &status);
+	if (rval) {
+		dev_err(&csi2->isys->adev->dev, "Acquire VSC failed.\n");
+		return rval;
+	}
+#endif
 
 	return rval;
 }
