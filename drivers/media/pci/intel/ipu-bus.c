3740a9e3fa7d8f91780f976488161d83036ad974
Hans de Goede <hdegoede@redhat.com>
2022-10-21 22:54:36 +0200

    ipu-bus: Split ipu_bus_add_device() into initialize() + add()
    Since all 3 the intel_ipu6, intel_ipu6_isys and intel_ipu6_psys modules
    use the same PCI modaliases to get automatically loaded, they can be loaded
    in any order. This means that one or both of the intel-ipu6-bus drivers can
    be loaded before ipu_pci_probe() creates the devices.
    
    This in turn means that the ipu_bus_device MUST be fully ready to be probed
    before device_add() is called for the ipu_bus_device.
    
    In order to make sure that the device is fully ready we must split
    ipu_bus_add_device() into separate initialize() + add() steps analog to
    how the driver-core has device_initialize() and device_add().
    
    This fixes the following 2 problems:
    
    1. ipu_bus_probe() calls pm_runtime_get_sync() this call would fail with
    -EACCESS if intel_ipu6_isys / intel_ipu6_psys were loaded already when
    ipu_pci_probe() runs because pm_runtime_enable() had not run yet when
    device_register() gets called and since the driver was already registered
    device_register() would immediately call ipu_bus_probe().
    
    To fix this call pm_runtime_forbid() (to temporarily disallow auto-suspend)
    and pm_runtime_enable() from the new ipu_bus_initialize_device(). So that
    runtime-pm is always enabled before the device_add() is done.
    
    While at it also add a missing pm_runtime_put() when pm_runtime_get_sync()
    fails, since pm_runtime_get_sync() always increases the pm_runtime count,
    even on failure.
    
    2. The pm_runtime_get_sync() issue can be fixed without splitting
    ipu_bus_add_device(), but then isys_probe() will crash with a NULL pointer
    dereference on adev->mmu.
    
    isys_probe() will run directly from the device_register() call and this
    runs before ipu_isys_init() calls isys->mmu = ipu_mmu_init(...).
    
    Fixing this requires splitting the ipu_bus device creation into
    initialize_device() + add_device() so that the ipu_bus_add_device() call
    can be delayed till after ipu_mmu_init() is done.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/media/pci/intel/ipu-bus.c
+++ drivers/media/pci/intel/ipu-bus.c
@@ -100,6 +100,7 @@ static int ipu_bus_probe(struct device *dev)
 	}
 	rval = pm_runtime_get_sync(&adev->dev);
 	if (rval < 0) {
+		pm_runtime_put(&adev->dev);
 		dev_err(&adev->dev, "Failed to get runtime PM\n");
 		goto out_err;
 	}
@@ -150,14 +151,13 @@ static void ipu_bus_release(struct device *dev)
 {
 }
 
-struct ipu_bus_device *ipu_bus_add_device(struct pci_dev *pdev,
-					  struct device *parent, void *pdata,
-					  struct ipu_buttress_ctrl *ctrl,
-					  char *name, unsigned int nr)
+struct ipu_bus_device *ipu_bus_initialize_device(struct pci_dev *pdev,
+						 struct device *parent, void *pdata,
+						 struct ipu_buttress_ctrl *ctrl,
+						 char *name, unsigned int nr)
 {
 	struct ipu_bus_device *adev;
 	struct ipu_device *isp = pci_get_drvdata(pdev);
-	int rval;
 
 	adev = devm_kzalloc(&pdev->dev, sizeof(*adev), GFP_KERNEL);
 	if (!adev)
@@ -183,20 +183,29 @@ struct ipu_bus_device *ipu_bus_add_device(struct pci_dev *pdev,
 	mutex_init(&adev->resume_lock);
 	dev_set_name(&adev->dev, "%s%d", name, nr);
 
-	rval = device_register(&adev->dev);
-	if (rval) {
-		put_device(&adev->dev);
-		return ERR_PTR(rval);
-	}
-
-	mutex_lock(&ipu_bus_mutex);
-	list_add(&adev->list, &isp->devices);
-	mutex_unlock(&ipu_bus_mutex);
-
-	pm_runtime_allow(&adev->dev);
+	device_initialize(&adev->dev);
+	pm_runtime_forbid(&adev->dev);
 	pm_runtime_enable(&adev->dev);
 
 	return adev;
+}
+
+int ipu_bus_add_device(struct ipu_bus_device *adev)
+{
+	int rval;
+
+	rval = device_add(&adev->dev);
+	if (rval) {
+		put_device(&adev->dev);
+		return rval;
+	}
+
+	mutex_lock(&ipu_bus_mutex);
+	list_add(&adev->list, &adev->isp->devices);
+	mutex_unlock(&ipu_bus_mutex);
+
+	pm_runtime_allow(&adev->dev);
+	return 0;
 }
 
 void ipu_bus_del_devices(struct pci_dev *pdev)
