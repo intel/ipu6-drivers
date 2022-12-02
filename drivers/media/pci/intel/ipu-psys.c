426fc5b548d726c4645217b4cd88293de443b1a9
zouxiaoh <xiaohong.zou@intel.com>
2022-12-02 15:25:44 +0800

    fix special kernel 5.15.71 build issue
    Signed-off-by: zouxiaoh <xiaohong.zou@intel.com>

--- drivers/media/pci/intel/ipu-psys.c
+++ drivers/media/pci/intel/ipu-psys.c
@@ -427,8 +427,8 @@ static int ipu_dma_buf_begin_cpu_access(struct dma_buf *dma_buf,
 	return -ENOTTY;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 70) \
-	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255) \
+	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 71)
 static int ipu_dma_buf_vmap(struct dma_buf *dmabuf, struct iosys_map *map)
 {
 	struct dma_buf_attachment *attach;
@@ -499,8 +499,8 @@ static void *ipu_dma_buf_vmap(struct dma_buf *dmabuf)
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 70) \
-	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255) \
+	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 71)
 static void ipu_dma_buf_vunmap(struct dma_buf *dmabuf, struct iosys_map *map)
 {
 	struct dma_buf_attachment *attach;
@@ -619,8 +619,8 @@ static inline void ipu_psys_kbuf_unmap(struct ipu_psys_kbuffer *kbuf)
 		return;
 
 	kbuf->valid = false;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 70) \
-	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255) \
+	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 71)
 	if (kbuf->kaddr) {
 		struct iosys_map dmap;
 
@@ -765,8 +765,8 @@ int ipu_psys_mapbuf_locked(int fd, struct ipu_psys_fh *fh,
 {
 	struct ipu_psys *psys = fh->psys;
 	struct dma_buf *dbuf;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 70) \
-	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255) \
+	|| LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 71)
 	struct iosys_map dmap;
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0) && LINUX_VERSION_CODE != KERNEL_VERSION(5, 10, 46)
 	struct dma_buf_map dmap;
@@ -1821,6 +1821,6 @@ MODULE_AUTHOR("Zaikuo Wang <zaikuo.wang@intel.com>");
 MODULE_AUTHOR("Yunliang Ding <yunliang.ding@intel.com>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Intel ipu processing system driver");
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 255)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0) || LINUX_VERSION_CODE == KERNEL_VERSION(5, 15, 71)
 MODULE_IMPORT_NS(DMA_BUF);
 #endif
