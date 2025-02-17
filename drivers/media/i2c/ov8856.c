1f64d8c9f075b07b1b4df579540ccd74f8f92707
Hao Yao <hao.yao@intel.com>
2025-02-17 17:22:18 +0800

    Update based on build 20250212_1315_368 for development
    Signed-off-by: Hao Yao <hao.yao@intel.com>

--- drivers/media/i2c/ov8856.c
+++ drivers/media/i2c/ov8856.c
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2021-2022 Intel Corporation.
 
+#include <linux/acpi.h>
+#include <linux/delay.h>
+#include <linux/i2c.h>
+#include <linux/module.h>
+#include <linux/pm_runtime.h>
 #include <linux/version.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 12, 0)
 #include <asm/unaligned.h>
 #else
 #include <linux/unaligned.h>
 #endif
-#include <linux/acpi.h>
-#include <linux/delay.h>
-#include <linux/i2c.h>
-#include <linux/module.h>
-#include <linux/pm_runtime.h>
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-fwnode.h>
