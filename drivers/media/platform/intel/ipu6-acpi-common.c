e004e22f20ab018a16db184da5e904bad29858f2
zouxiaoh <xiaohong.zou@intel.com>
2025-07-02 17:07:38 +0800

    code changes for link frequency and sensor physical address
    Signed-off-by: zouxiaoh <xiaohong.zou@intel.com>

--- drivers/media/platform/intel/ipu6-acpi-common.c
+++ drivers/media/platform/intel/ipu6-acpi-common.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2016--2024 Intel Corporation.
+ * Copyright (c) 2016-2024 Intel Corporation.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
